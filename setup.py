e9bb8232127dbf50c33a0c5872097f46a6bbd8a7
Felipe Sabino <sabino@users.noreply.github.com>
2019-02-28 11:58:48 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
         'tiendanube',
         'tiendanube.resources',
     ],
-    version = '1.1',
+    version = '1.2',
     description = '',
     license = 'MIT',
     long_description = read('README.rst'),
